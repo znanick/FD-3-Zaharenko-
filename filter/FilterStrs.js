fc23e5077b3c61fabc45d0d9e469b8cd6c68c088
znanick <znanick@gmail.com>
2020-07-08 19:44:13 +0300

    add homeWork 2 filter

---
+++ filter/FilterStrs.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var FilterStrs = React.createClass({
+  displayName: "FilterStrs",
+
+  getInitialState: function () {
+    return {
+      sortState: false,
+      filterState: "",
+      strsView: [],
+    };
+  },
+
+  filterList: function () {
+    let str = this.props.strs.slice();
+    if (this.state.filterState) {
+      str = str.filter((s) => s.indexOf(this.state.filterState) != -1);
+    }
+    if (this.state.sortState) {
+      str.sort();
+    }
+    this.setState({ strsView: str },);
+  },
+
+  sortStr:function(eo){
+    this.setState({sortState: eo.target.checked}, this.filterList)
+  },
+
+  filterStr: function(eo){
+    this.setState({filterState: eo.target.value}, this.filterList)
+  }, 
+
+  buttonReset: function(){
+    this.setState({sortState:false, filterState:''})
+  },
+
+  render: function () {
+    this.filterList() 
+
+    return React.DOM.div(
+      { className: "Filter" },
+      React.DOM.input({ type:'checkbox', onClick:this.sortStr, value:this.state.sortState}),
+      React.DOM.input({ type:'text', onChange:this.filterStr, value:this.state.filterState}),
+      React.DOM.button({  onClick:this.buttonReset}, "Сброс"),
+      React.DOM.div ({className:'textArea'}, this.state.strsView)
+    );
+  },
+});
