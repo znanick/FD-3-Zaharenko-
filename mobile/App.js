9b421279d6b05797b23aa0fe176c7f7a24c9e6cd
znanick <znanick@gmail.com>
2020-09-02 11:48:28 +0300

    add hw mobile

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ClientsPage from "./components/ClientsPage";
+
+var clientList = require("./clientList.json");
+var companyList = ["A1", "MTS"];
+
+ReactDOM.render(
+  <ClientsPage companyList={companyList} catalog={clientList} />,
+  document.getElementById("container")
+);
---
+++ mobile/components/ClientsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./ClientsPage.css";
+
+import Client from "./Client";
+
+
+class ClientsPage extends React.PureComponent {
+  
+}
+
+export default ClientsPage;
